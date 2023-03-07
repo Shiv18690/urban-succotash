d93e2007bce5673264fdd0d08e1cf13994f021f0
Shivashish <shiv18690@gmail.com>
2023-03-07 14:26:10 +0100

    refactored some more

--- src/main/java/LeapTest.java
+++ src/main/java/LeapTest.java
@@ -5,15 +5,8 @@ public  class LeapTest {
     public  static boolean isLeap(Year year) {
 
         int yearL = getYear(year);
-/**
- if(yearL % 400 == 0 )
- {
- return true;
- }
- */
-        if (yearL % 100 == 0 && yearL % 400 == 0) {
-            return true;
-        } else if (yearL % 4 == 0 && yearL % 100 != 0) {
+
+        if ((yearL % 100 == 0 && yearL % 400 == 0) ||  (yearL % 4 == 0 && yearL % 100 != 0)) {
             return true;
         }
         return false;
--- src/test/java/LeapTestTest.java
+++ src/test/java/LeapTestTest.java
@@ -1,35 +1,39 @@
+import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
 import org.junit.rules.ExpectedException;
-
-
+import org.junit.runner.RunWith;
+import junitparams.JUnitParamsRunner;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
-
+@RunWith(JUnitParamsRunner.class)
 public class LeapTestTest {
 
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
     @Test
-    public void isLeap(){
+    @Parameters({"2000"})
+    public void isLeap(int year){
 
-    assertThat("is the year Leap",LeapTest.isLeap(new Year(2000)));
+        //System.out.println(year);
+    assertThat("is the year Leap",LeapTest.isLeap(new Year(year)));
 
 
 
     }
 
     @Test
-    public void notALeap() throws Exception{
+    @Parameters({"1700","1800","1900","2100","2017","2018","2019"})
+    public void notALeap(int year) throws Exception{
 
-
+        //System.out.println(year);
         thrown.expect(AssertionError.class);
-        thrown.expectMessage("is the year Leap");
-        assertThat("is the year Leap",LeapTest.isLeap(new Year(1700)));
+        thrown.expectMessage("year is leap");
+        assertThat("year is leap",LeapTest.isLeap(new Year(year)));
 
         //LeapTest.isLeap(new Year(1700));
 
@@ -43,9 +47,10 @@ public class LeapTestTest {
 
 
     @Test
-    public void isLeapSpecialYears(){
+    @Parameters({"1852","2008","2012","2016"})
+    public void isLeapSpecialYears(int year){
 
-        assertThat("is the year Leap",LeapTest.isLeap(new Year(1852)));
+        assertThat("is the year Leap",LeapTest.isLeap(new Year(year)));
 
 
 
